7b8dd7a4ca2d80dce9b61e4d30caddf743873e81
JakkapatB <jakkapat.bo@kkumail.com>
2024-09-05 09:52:03 +0700

    Create a form structure

--- app/controllers/user_registers_controller.rb
+++ app/controllers/user_registers_controller.rb
@@ -1,13 +1,24 @@
 class UserRegistersController < ApplicationController
   def new
+    @user_register = UserRegister.new
   end
 
   def create
+    @user_register = UserRegister.new(user_register_params)
+    if @user_register.save
+      redirect_to root_path, notice: 'User was successfully created.'
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def delete
+  end
+  private
+  def user_register_params
+    params.require(:user_register).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
   end
 end
